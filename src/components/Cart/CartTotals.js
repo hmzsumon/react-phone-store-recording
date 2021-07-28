c5849cc8e331a0e8a6367148ae8186c41092fc14
Zaklaria <zsumonn@gmail.com>
2021-07-28 12:04:38 +0600

    corection error

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -5,7 +5,7 @@ export default class CartTotals extends Component {
 	render() {
 		const { cartSubTotal, cartTax, cartTotal, cart, clearCart } =
 			this.props.value;
-		const { history } = this.props;
+		// const { history } = this.props;
 		const emptyCart = cart.length === 0 ? true : false;
 		return (
 			<React.Fragment>
